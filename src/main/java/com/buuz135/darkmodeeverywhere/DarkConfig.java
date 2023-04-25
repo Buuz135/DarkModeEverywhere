d7e9662f5b22b5a7f0b9f77ce23b6f0be70d0adc
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2023-04-25 02:37:22 +0100

    fix unintended config nesting introduced in version 1.19.2-1.1.1

--- src/main/java/com/buuz135/darkmodeeverywhere/DarkConfig.java
+++ src/main/java/com/buuz135/darkmodeeverywhere/DarkConfig.java
@@ -55,13 +55,14 @@ public class DarkConfig {
                 .translation(TRANSLATION_KEY_BASE + "method_shader_dump")
                 .define("METHOD_SHADER_DUMP", false);
 
-            BUILDER.push("Button Position");
+            BUILDER.push("Inventory Button");
             GUI_BUTTON_X_OFFSET = BUILDER.comment("Pixels away from the left of the GUI in the x axis")
                 .translation(TRANSLATION_KEY_BASE + "button_position_x")
                 .defineInRange("X", 4, 0, Integer.MAX_VALUE);
             GUI_BUTTON_Y_OFFSET = BUILDER.comment("Pixels away from the bottom of the GUI in the y axis")
                 .translation(TRANSLATION_KEY_BASE + "button_position_y")
                 .defineInRange("Y", 0, 0, Integer.MAX_VALUE);
+            BUILDER.pop();
 
             BUILDER.push("Main Menu Button");
             SHOW_BUTTON_IN_TITLE_SCREEN = BUILDER.comment("Enabled")
@@ -73,6 +74,7 @@ public class DarkConfig {
             TITLE_SCREEN_BUTTON_Y_OFFSET = BUILDER.comment("Pixels away from the bottom of the GUI in the y axis")
                 .translation(TRANSLATION_KEY_BASE + "button_position_y")
                 .defineInRange("MAIN_Y", 40, 0, Integer.MAX_VALUE);
+            BUILDER.pop();
 
             SPEC = BUILDER.build();
         }
