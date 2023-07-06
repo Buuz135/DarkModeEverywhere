ec43250d23443edcb1915496abea2594e914b34a
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2023-07-06 22:55:49 +0200

    Implement a mixin to set parameters for DarkShaderInstances

---
+++ src/main/java/com/buuz135/darkmodeeverywhere/mixins/VertexBufferMixin.java
@@ -0,0 +1,24 @@
+package com.buuz135.darkmodeeverywhere.mixins;
+
+import com.buuz135.darkmodeeverywhere.ClientProxy;
+import com.buuz135.darkmodeeverywhere.DarkShaderInstance;
+import com.mojang.blaze3d.vertex.VertexBuffer;
+import net.minecraft.client.renderer.ShaderInstance;
+import org.joml.Matrix4f;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(VertexBuffer.class)
+public class VertexBufferMixin {
+
+    @Inject(method="_drawWithShader", at=@At(value="INVOKE", target="Lcom/mojang/blaze3d/systems/RenderSystem;setupShaderLights(Lnet/minecraft/client/renderer/ShaderInstance;)V"))
+    private void _drawWithShader(Matrix4f p_253705_, Matrix4f p_253737_, ShaderInstance p_166879_, CallbackInfo ci) {
+        if (!(p_166879_ instanceof DarkShaderInstance darkShaderInstance)) return;
+
+        if (darkShaderInstance.DivideFactor == null) return;
+
+        darkShaderInstance.DivideFactor.set(ClientProxy.getSelectedShaderValue().divideFactor);
+    }
+}
