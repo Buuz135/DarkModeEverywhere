0238c28aa6b1c4c6e82c79ca1d87f2f3fa87f4b0
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2023-07-07 01:14:37 +0200

    Checkout ClassUtil from 1.20

---
+++ src/main/java/com/buuz135/darkmodeeverywhere/ClassUtil.java
@@ -0,0 +1,33 @@
+package com.buuz135.darkmodeeverywhere;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import cpw.mods.modlauncher.api.INameMappingService;
+import net.minecraft.client.gui.GuiComponent;
+import net.minecraft.client.renderer.GameRenderer;
+import net.minecraftforge.fml.util.ObfuscationReflectionHelper;
+
+import java.util.stream.Stream;
+
+public class ClassUtil {
+
+    private static final String SET_SHADER_METHOD_NAME = ObfuscationReflectionHelper.remapName(INameMappingService.Domain.METHOD, "m_157427_");
+    private static String walkForCallerClassName(Stream<StackWalker.StackFrame> stackFrameStream) {
+        return stackFrameStream
+                .filter(frame -> !(
+                        frame.getClassName().equals(GameRenderer.class.getName())
+                                || frame.getClassName().equals(ClassUtil.class.getName())
+                                || frame.getClassName().equals(RenderSystem.class.getName())
+                                || frame.getClassName().startsWith("java.lang.Thread")
+                                || frame.getMethodName().equals(SET_SHADER_METHOD_NAME)
+                                || frame.getClassName().equals(GuiComponent.class.getName())
+                ))
+                .findFirst()
+                .map(f -> f.getClassName() + ":" + f.getMethodName())
+                .orElse(null);
+    }
+
+    public static String getCallerClassName() {
+        return StackWalker.getInstance()
+                .walk(ClassUtil::walkForCallerClassName);
+    }
+}
