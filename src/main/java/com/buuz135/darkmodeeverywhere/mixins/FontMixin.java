a4468588e60d3a66da00c67c280fb0fca31b3f07
Buuz135 <buuz135@hotmail.com>
2024-11-20 16:13:04 +0100

    Fixed text color replacement not working properly in some cases or in some shades of grey like the default ones

--- src/main/java/com/buuz135/darkmodeeverywhere/mixins/FontMixin.java
+++ src/main/java/com/buuz135/darkmodeeverywhere/mixins/FontMixin.java
@@ -2,14 +2,13 @@ package com.buuz135.darkmodeeverywhere.mixins;
 
 import com.buuz135.darkmodeeverywhere.ClientProxy;
 import com.buuz135.darkmodeeverywhere.ShaderConfig;
-import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.util.FastColor;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
 
 
 @Mixin(Font.class)
@@ -17,18 +16,20 @@ public class FontMixin {
 
     @Inject(method = "adjustColor", at = @At(value = "HEAD", target = "Lnet/minecraft/client/gui/Font;adjustColor(I)I"), cancellable = true)
     private static void adjustColorA(int color, CallbackInfoReturnable<Integer> cir) {
-        if (ClientProxy.SELECTED_SHADER_VALUE != null && Minecraft.getInstance().screen != null) {
-            int threshold = 65;
-            ShaderConfig.ShaderValue shaderValue = ClientProxy.SELECTED_SHADER_VALUE;
+        if (ClientProxy.SELECTED_SHADER != null && Minecraft.getInstance().screen != null) {
+            if (color == 0) return;
+            int thre = 65;
+            ShaderConfig.ShaderValue shaderValue = ClientProxy.SHADER_VALUES.get(ClientProxy.SELECTED_SHADER);
             if (shaderValue.darkColorReplacement == -1) return;
             if (ChatFormatting.GRAY.getColor().equals(color) || ChatFormatting.DARK_GRAY.getColor().equals(color)) {
                 cir.setReturnValue(0xFF000000 | shaderValue.darkColorReplacement);
                 return;
             }
-            if (FastColor.ARGB32.red(color) < threshold && FastColor.ARGB32.green(color)  < threshold && FastColor.ARGB32.blue(color)  < threshold){
+            if (FastColor.ARGB32.red(color) < thre && FastColor.ARGB32.green(color)  < thre && FastColor.ARGB32.blue(color)  < thre){
                 cir.setReturnValue(0xFF000000 | shaderValue.darkColorReplacement);
                 return;
             }
         }
     }
+
 }
