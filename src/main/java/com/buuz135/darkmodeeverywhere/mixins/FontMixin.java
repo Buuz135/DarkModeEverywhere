309b7160c7b451cd990c9366691b244445dd1179
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-04-09 19:51:26 +0100

    rename `thre` -> `threshold`
    this is 2025, we don't need to minimise character usage

--- src/main/java/com/buuz135/darkmodeeverywhere/mixins/FontMixin.java
+++ src/main/java/com/buuz135/darkmodeeverywhere/mixins/FontMixin.java
@@ -10,7 +10,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 
-
 @Mixin(Font.class)
 public class FontMixin {
 
@@ -18,14 +17,14 @@ public class FontMixin {
     private static void adjustColorA(int color, CallbackInfoReturnable<Integer> cir) {
         if (ClientProxy.SELECTED_SHADER != null && Minecraft.getInstance().screen != null) {
             if (color == 0) return;
-            int thre = 65;
+            int threshold = 65;
             ShaderConfig.ShaderValue shaderValue = ClientProxy.SHADER_VALUES.get(ClientProxy.SELECTED_SHADER);
             if (shaderValue.darkColorReplacement == -1) return;
             if (ChatFormatting.GRAY.getColor().equals(color) || ChatFormatting.DARK_GRAY.getColor().equals(color)) {
                 cir.setReturnValue(0xFF000000 | shaderValue.darkColorReplacement);
                 return;
             }
-            if (FastColor.ARGB32.red(color) < thre && FastColor.ARGB32.green(color)  < thre && FastColor.ARGB32.blue(color)  < thre){
+            if (FastColor.ARGB32.red(color) < threshold && FastColor.ARGB32.green(color)  < threshold && FastColor.ARGB32.blue(color)  < threshold){
                 cir.setReturnValue(0xFF000000 | shaderValue.darkColorReplacement);
                 return;
             }
